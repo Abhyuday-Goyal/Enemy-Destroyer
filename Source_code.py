eee60423a403d95901ae4325b7e04561b1270508
Abhyuday-Goyal <abhyuday@ainorthstartech.com>
2022-09-01 23:34:25 +0530

    changes

--- Source_code.py
+++ Source_code.py
@@ -4,3 +4,7 @@ pygame.init()
 screen = pygame.display.set_mode((800,600))#800 is the x value and 600 is the y value
 pygame.display.set_caption("Space Invaders")
 playerimg = pygame.image.load("shooter.png")
+playerX = 370
+playerY = 480
+playerX_change = 0
+enemyimg = pygame.image.load("enemy.png")
