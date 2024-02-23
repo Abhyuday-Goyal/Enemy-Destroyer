496fa48129ce0c4b62b7d152e527bbe87df23ffd
Abhyuday Goyal <abhyuday2106@gmail.com>
2024-02-23 12:11:39 -0500

    Name change

--- Source_code.py
+++ Source_code.py
@@ -3,7 +3,7 @@ import random
 import math
 pygame.init()
 screen = pygame.display.set_mode((800,600))#800 is the x value and 600 is the y value
-pygame.display.set_caption("Space Invaders")
+pygame.display.set_caption("Enemy Destroyer")
 playerimg = pygame.image.load("shooter.png")
 playerX = 370
 playerY = 480
